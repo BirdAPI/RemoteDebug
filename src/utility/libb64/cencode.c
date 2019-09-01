d90160a5c7dae8a8322b5c8748515bbba690d97e
BirdAPI <birdapi@gmail.com>
2019-08-31 21:23:36 -0700

    include c_types.h

--- src/utility/libb64/cencode.c
+++ src/utility/libb64/cencode.c
@@ -5,6 +5,8 @@ This is part of the libb64 project, and has been placed in the public domain.
 For details, see http://sourceforge.net/projects/libb64
 */
 
+#include <c_types.h>
+
 #ifdef ESP8266
 #include <core_esp8266_features.h>
 #endif
